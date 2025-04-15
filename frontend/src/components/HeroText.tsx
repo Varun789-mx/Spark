a587cbea3a74b8b42c1a03a13595ea4c9cb38782
varun789-mx <varunsh153@gmail.com>
2025-04-16 01:14:09 +0530

    Add the background and text and input

---
+++ frontend/src/components/HeroText.tsx
@@ -0,0 +1,27 @@
+import { InputBox } from "./Inputbox"
+
+
+export const HeroText = () => {
+
+
+  return (
+    <>
+      <div className='min-w-screen min-h-screen bg-transparent  flex justify-center items-center'>
+        <div className='text-white font-serif'>
+          <span className='text-4xl'>Prompt , build ,Ship in minutes</span>
+          <div className="pl-4">
+            <span className='text-xl text-red-400'>   Build you favourite Apps for
+            </span>
+            <span className='text-xl text-white'> Mobile</span>
+            <span className='text-xl text-red-400'> and</span>
+            <span className='text-xl text-white'> Desktop</span>
+          </div>
+          <InputBox />
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
