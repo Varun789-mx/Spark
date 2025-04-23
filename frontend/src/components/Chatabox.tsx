7dee98d6b76eb9d9fa922cbdb48811afd56c80cf
varun789-mx <varunsh153@gmail.com>
2025-04-23 23:33:41 +0530

    Add Chatbox.tsx

--- frontend/src/components/Chatabox.tsx
+++ frontend/src/components/Chatabox.tsx
@@ -1,34 +1,29 @@
+import axios from "axios";
 import { useState } from "react"
+import { BACKEND_URL } from "../config";
+
 
 export const Chatbox = () => {
     const [chat, SetChat] = useState("");
-    return <>
-        <div className="w-full max-w-3xl mx-auto p-4">
-            <label className="block text-white text-lg font-semibold mb-2">
-                Describe your website
-            </label>
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (chat.trim()) {
+            axios.put(`${BACKEND_URL}/chat`, {
+                prompt: chat
+            }).then((res) => {
+                console.log(res);
+            }).catch(error => console.log(error));
+        }
+
+    }
+    return (
+        <div className="w-full max-w-3xl mx-auto">
+            <label className="block text-white text-lg font-semibold mb-2">Describe your changes</label>
             <div className="relative">
-                <textarea
-                    maxLength={500}
-                    placeholder="Create an elegant portfolio website for a photographer with a dark theme..."
-                    className="w-full h-40 bg-[#0D1117] text-white p-4 rounded-xl border border-[#3D3D3D] focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
-                />
-                <span className="absolute bottom-2 right-3 text-sm text-gray-500">
-                    0/500
-                </span>
+                <form className="space-y-4" onSubmit={handleSubmit} >
+                    <textarea value={chat} onChange={(e) => SetChat(e.target.value)} maxLength={500} className="w-full h-40 bg-[#0d1117] text-white p-4 rounded-xl border border-[#3D3D3D] focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none" />
+                </form>
             </div>
         </div>
-
-    </>
-
-}
-export function dee() {
-    return (<>
-        <div className=" w-full max-w-3xl mx-auto p-4 ">
-                <h2 className="block text-lg font-medium text-white p-2 ">Describe your Changes</h2>
-                <textarea className="w-1/3 h-1/4 rounded-2xl bg-slate-900 text-slate-400 "
-                    value={chat} onChange={(e) => SetChat(e.target.value)} />
-            </div>
-
-        </div></>)
+    )
 }
