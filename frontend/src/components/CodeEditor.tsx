f4196ad053cbde6934db4f3a20254e82a46ffee3
varun789-mx <varunsh153@gmail.com>
2025-06-08 00:05:38 +0530

    Fix the alignment of the Code Editor

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,30 +1,33 @@
+  import Editor, { loader } from '@monaco-editor/react';
+  import { FileExplorer } from './FileExplorer';
 
-import Editor, { loader } from '@monaco-editor/react';
+  loader.init().then((monaco) => {
+      monaco.editor.defineTheme('myTheme', {
+          base: 'vs-dark',
+          inherit: true,
+          rules: [],
+          colors: {
+              'editor.background': '#0d1117',
+          },
+      });
+  });
+  function CodeEditor() {
+  
 
-loader.init().then((monaco) => {
-    monaco.editor.defineTheme('myTheme', {
-        base: 'vs',
-        inherit: true,
-        rules: [],
-        colors: {
-            'editor.background': '#0d1117',
-        },
-    });
-});
-function CodeEditor() {
- 
-  return (
-    <div  className='w-full'> 
-      {" "}
-      <Editor  
-        height="90vh"
-        theme="myTheme"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
-      />
-      ;
-    </div>
-  );
-}
 
-export default CodeEditor;
+    return (
+      <div  className='w-2/3  '> 
+        {" "}
+       
+        <Editor  
+          height="90vh"
+          theme="myTheme"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+        />
+        ;
+      </div>
+    );
+  }
+
+  export default CodeEditor;
--- frontend/src/pages/Builder.tsx
+++ frontend/src/pages/Builder.tsx
@@ -1,27 +1,26 @@
-import { Background } from "../components/Background";
-import { Chatbox } from "../components/Chatabox";
-import CodeEditor from "../components/CodeEditor";
 import { Navbar } from "../components/Navbar";
-import { ResponseDisplay } from "../components/ResponseDisplay";
-
+import { Chatbox } from "../components/Chatabox";
+import { Background } from "../components/Background";
+import CodeEditor from "../components/CodeEditor";
 
 
 export function Builder() {
     return (
- <div className=" relative min-h-screen">
+        <div className=" relative min-h-screen">
             <div className="fixed inset-0 -z-10">
                 <Background />
-            </div> 
-            <div className="relative -z-10 flex flex-col ">
+            </div>
+            <div className="relative z-10  ">
                 <Navbar />
-   
-<div className="w-full h-screen pt-5">
-                    <div className="absolute top-3/4  left-8 flex-1 items-center ">
-                        <Chatbox />
-                    </div>
-             </div>
+            </div>
+            <div className="flex w-full justify-items-center px-6 m-2">
+                <div className="absolute  w-1/3 top-3/4 flex-1 items-center p-2 ">
+                    <Chatbox />
+                </div>
+               <div className="absolute top-16 right-0 w-3/5 p-2">
+    <CodeEditor />
+</div>
             </div>
         </div>
     )
-
 }
