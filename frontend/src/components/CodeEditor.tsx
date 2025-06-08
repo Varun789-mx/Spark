5578eeba813f131ebc01acbb2f417b91fd2f1c82
varun789-mx <varunsh153@gmail.com>
2025-06-08 21:37:09 +0530

    Fix the layout of the file editor

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -2,6 +2,7 @@ import Editor, { loader } from '@monaco-editor/react';
 import { FileExplorer } from './FileExplorer';
 import type { FileItem } from './types';
 import { useState } from 'react';
+import { Toggle } from './ui/toggle';
 
 loader.init().then((monaco) => {
   monaco.editor.defineTheme('myTheme', {
@@ -49,30 +50,37 @@ const sampleFiles: FileItem[] = [
 
 function CodeEditor() {
   const [selectedFile, setSelectedFile] = useState<FileItem | null>(null);
+  const [isPressed, setIsPressed] = useState(false)
 
   const handleFileSelect = (file: FileItem) => {
     setSelectedFile(file);
     console.log("Selected file:", file);
   };
   return (
-    <div>
-      <div>
-        <div className=''>
-        <button className='text-gray-500 font-semibold bg-black rounded-lg p-1 text-sm focus:bg-gray-800'>Code</button>
-        <button className='text-gray-500 bg-black font-semibold rounded-xl p-1 text-sm focus:bg-gray-800'>preview</button>
+    <div className='bg-neutral-900 rounded-lg p-2 border-2 border-blue-900 h-[87vh]'>
+      <div >
+        <div className=" rounded-sm m-4 p-1  w-fit bg-gray-800">
+          <button onClick={() => setIsPressed(!isPressed)} className={` font-semibold w-15 rounded-lg p-1 text-sm  ${isPressed ? "bg-black text-white" : "text-gray-500"}`}>Code</button>
+          <button onClick={() => setIsPressed(!isPressed)} className={` font-semibold w-15 rounded-lg p-1 text-sm  ${!isPressed ? "bg-black text-white" : "text-gray-500"}`}>Preview</button>
+        </div>
       </div>
-      </div>
-      <div className='w-full flex justify-center bg-black '>
+      <div className='w-full flex justify-center bg-black rounded-lg border-2'>
         {" "}
+        <div className='w-1/3'>
+          <FileExplorer files={sampleFiles} onFileSelect={handleFileSelect} />
+        </div>
+        <div className='w-full'>
+          {isPressed ?
+            <Editor
+              height="70vh"
+              theme="myTheme"
+              defaultLanguage="javascript"
+              defaultValue="// some comment"
+            />
+            : <div className='h-[70vh]'>Empty</div>
+          }
+        </div>
 
-        <FileExplorer files={sampleFiles} onFileSelect={handleFileSelect} />
-        <Editor
-          height="87vh"
-          theme="myTheme"
-          defaultLanguage="javascript"
-          defaultValue="// some comment"
-        />
-        ;
       </div>
     </div>
   );
