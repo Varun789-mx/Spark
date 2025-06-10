14e94520679ba1e232e8482b1ca3c52cdbb61e31
Harsh <VARUNSH153@GMAIL.COM>
2025-06-10 16:15:56 +0530

    Fix the steplist comp

--- frontend/src/components/Steps.tsx
+++ frontend/src/components/Steps.tsx
@@ -8,20 +8,20 @@ interface SteplistProp {
 }
 export function Steplist({ step, currentstep, onStepClick }: SteplistProp) {
     return (
-        <div className="bg-black">
-            <div className="bg-gray-800">
-                <h1>Steps in Process</h1>
+        <div className="bg-black w-2/3 items-center p-4 rounded-lg">
+            <div className="bg-gray-900">
+                <h1 className="text-white">Steps in Process</h1>
                 {step.map((step) => (
                     <div onClick={() => onStepClick(step.id)} key={step.id}
                         className={`${currentstep === step.id ? 'bg-gray-700' :
                             'bg-gray-800'}`} >
-                        <div>
+                        <div className="flex justify-items-center p-4 gap-4 ">
                             {step.status === 'Completed' ?
                                 (<CheckCircle className="bg-yellow-200" />) :
                                 step.status === 'In-progress' ?
                                     (<Clock className="bg-green-500" />) :
                                     (<Circle className="bg-gray-800" />)}
-                            <h3>{step.title}</h3>
+                            <h3 className="text-white">{step.title}</h3>
                         </div>
                     </div>
                 ))}
--- frontend/src/pages/Builder.tsx
+++ frontend/src/pages/Builder.tsx
@@ -8,62 +8,64 @@ import { useEffect, useState } from "react";
 import type { Step } from "../components/types";
 import { Parsexml } from "../Steps";
 import { Steplist } from "../components/Steps";
+import { Editor } from "../components/Editor";
 
 export function Builder() {
-    const location = useLocation();
-    const Stateprompt = location.state?.prompt;
-    const [steps, setSteps] = useState<Step[]>([]);
-    const [currentStep, setCurrentStep] = useState<number>(0);
-    const [prompts, setPrompts] = useState<string>("");
+  const location = useLocation();
+  const Stateprompt = location.state?.prompt;
+  const [steps, setSteps] = useState<Step[]>([]);
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [prompts, setPrompts] = useState<string>("");
 
-    const { prompt } = location.state as { prompt: string };
+  const { prompt } = location.state as { prompt: string };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.post(`${BACKEND_URL}/template`, {
-                    prompt: Stateprompt.trim(),
-                });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(`${BACKEND_URL}/template`, {
+          prompt: Stateprompt.trim(),
+        });
 
-                const [promptsData, uiprompt] = response.data;
-                setPrompts(promptsData);
-                const parsedSteps = Parsexml(uiprompt[0]);
-                setSteps(parsedSteps);
-                if (parsedSteps.length > 0) {
-                    setCurrentStep(parsedSteps[0].id);
-                }
-            } catch (error) {
-                console.error("Error loading steps:", error);
-            }
-        };
+        const { promptsData, uiprompt } = response.data;
+        setPrompts(promptsData);
+        const parsedSteps = Parsexml(uiprompt);
+        setSteps(parsedSteps);
+        if (parsedSteps.length > 0) {
+          console.log(parsedSteps);
+          setCurrentStep(parsedSteps[0].id);
+          console.log(currentStep);
+          setSteps(parsedSteps);
+        }
+      } catch (error) {
+        console.error("Error loading steps:", error);
+      }
+    };
 
-        fetchData();
-    }, [prompt]);
+    fetchData();
+  }, [prompt]);
 
-    return (
-        <div className="relative min-h-screen">
-            <span className="hidden">{prompts}</span>
-            <div className="fixed inset-0 -z-10">
-                <Background />
-            </div>
-            <div className="relative z-10">
-                <Navbar />
-            </div>
-            <div className="flex w-full justify-items-center px-6 m-2">
-                <div className="absolute w-1/3 top-3/4 flex-1 flex-col items-center p-2">
-                    <Steplist
-                        step={steps}
-                        currentstep={currentStep}
-                        onStepClick={setCurrentStep}
-                    />
-                    <Chatbox />
-                </div>
-                <div className="absolute top-16 right-0 w-3/5 p-2">
-                    {steps.map((res) => (
-                        <span key={res.id}>{res.description}</span>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="relative min-h-screen">
+      <span className="hidden">{prompts}</span>
+      <div className="fixed inset-0 -z-10">
+        <Background />
+      </div>
+      <div className="relative z-10">
+        <Navbar />
+      </div>
+      <div className="flex w-full justify-items-center px-6 m-2">
+        <div className="absolute w-1/3 top-3/4 flex-1 flex-col items-center p-2">
+          <Steplist
+            step={steps}
+            currentstep={currentStep}
+            onStepClick={setCurrentStep}
+          />
+          <Chatbox />
         </div>
-    );
+        <div className="absolute top-16 right-0 w-3/5 p-2">
+          <Editor />
+        </div>
+      </div>
+    </div>
+  );
 }
