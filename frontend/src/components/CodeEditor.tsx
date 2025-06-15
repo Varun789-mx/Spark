cca0cea8bacbfafc6d16090c5058ffa9802f5282
varun789-mx <varunsh153@gmail.com>
2025-06-15 23:59:20 +0530

    Remove the mapping from CodeEditor.tsx

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,41 +1,36 @@
 import { Editor } from "@monaco-editor/react";
-import type{ FileItem } from "./types";
+import type { FileItem } from "./types";
 
 interface CodeEditorprop {
-  File: FileItem[] ,
+  File: FileItem,
 }
 
 
 
-export function CodeEditor({File}:CodeEditorprop) { 
-    if (!File) {
+export function CodeEditor({ File }: CodeEditorprop) {
+  if (!File) {
     return (
       <div className='h-full flex items-center justify-center text-gray-400'>
         Please Select a file to view its content
       </div>
     )
   }
-    return (
-      <div>
-       {File.map((file)=> (
-
-      <div key={file.name}>
-     <Editor
-              height="70vh"
-              theme="vs-dark"
-              defaultLanguage="typescript"
-              defaultValue="// some comment"
-              value={file?.content || ""}
-              options={{
-                readOnly: true,
-                minimap: { enabled: false },
-                fontSize: 14,
-                wordWrap: "on",
-                scrollBeyondLastLine: false,
-              }}
-            />
-            </div>
-       ))}
-</div>
-    )     
-        }
+  return (
+    <div>
+          <Editor
+            height="70vh"
+            theme="vs-dark"
+            defaultLanguage="typescript"
+            defaultValue="// some comment"
+            value={File?.content || ""}
+            options={{
+              readOnly: true,
+              minimap: { enabled: false },
+              fontSize: 14,
+              wordWrap: "on",
+              scrollBeyondLastLine: false,
+            }}
+          />
+        </div>
+  )
+}
