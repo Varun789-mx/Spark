33323e3939313a0043b9fea77dd405c1fb78ddc2
varun789-mx <varunsh153@gmail.com>
2025-07-06 14:26:37 +0530

    Add the enter to submit thing

--- frontend/src/components/Chatabox.tsx
+++ frontend/src/components/Chatabox.tsx
@@ -5,14 +5,28 @@ import { BACKEND_URL } from "../config";
 
 export const Chatbox = () => {
     const [chat, SetChat] = useState("");
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+    const [loading, setloading] = useState(false);
+    const handleSubmit = () => {
         if (chat.trim()) {
-            axios.put(`${BACKEND_URL}/chat`, {
+            setloading(true);
+            axios.post(`${BACKEND_URL}/chat`, {
                 prompt: chat
             }).then((res) => {
+
                 console.log(res);
-            }).catch(error => console.log(error));
+            }).catch(error =>
+                console.log(error)).finally(() => {
+                    setloading(false);
+                });
+        }
+    }
+
+    const HandleRequest = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            console.log(loading, "From loading ")
+
+            handleSubmit();
         }
 
     }
@@ -20,8 +34,8 @@ export const Chatbox = () => {
         <div className="w-full max-w-7xl mx-auto p-2">
             <label className="block text-white text-lg font-semibold mb-2">Describe your changes</label>
             <div className="relative">
-                <form className="space-y-4" onSubmit={handleSubmit} >
-                    <textarea value={chat} onChange={(e) => SetChat(e.target.value)} maxLength={500}
+                <form className="space-y-4" >
+                    <textarea value={chat} onChange={(e) => SetChat(e.target.value)} onKeyDown={HandleRequest} disabled={loading} maxLength={500}
                         className="w-md h-30 bg-[#0d1117] 
                     text-white p-4 rounded-lg border 
                     border-[#3D3D3D] focus:outline-none 
